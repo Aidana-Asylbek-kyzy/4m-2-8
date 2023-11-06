a4c3fc65ce2e7f001f98c9146d02d494526f6d5f
Aidana-Asylbek-kyzy <aidanaasylbekkyzy12@gmail.com>
2023-11-06 16:46:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import Input from "./Component/Input";
+import Button from "./Component/Button";
+import Switcher from "./Component/Switcher";
+import Todoitem from "./Component/Todoitem";
+
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [Todos, setTodos] = useState([]);
+
+  const addTask = () => {
+    console.log("Добавление задачи");
+    console.log("title:", title);
+    console.log("description:", description);
+  
+    if (title.trim() !== "") {
+      const newTask = { title, description };
+      setTodos([...Todos, newTask]);
+      setTitle("");
+      setDescription("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input"> 
+        
+        <Input
+          title={title}
+          description={description}
+          onTitleChange={setTitle}
+          onDescriptionChange={setDescription}
+        />
+
+        <Button addtask={addTask} />
+
+        </div>
+
+        <Switcher />
+        <div className="todo-list">
+
+        {Todos.map((todo, index) => (
+          <Todoitem key={index} title={todo.title} description={todo.description} />
+        ))}
+
+        </div>
+      </div>
     </div>
   );
 }
